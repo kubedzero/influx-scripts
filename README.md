3d08c92bb2fe28ee7ee854bce1dac145cab000c8
KZ <kubedzero@gmail.com>
2019-09-29 07:09:22 -0700

    Cleanup and update of code for ESP reading

---
+++ README.md
@@ -0,0 +1,3 @@
+* https://itnext.io/upgrading-bash-on-macos-7138bd1066ba explains why Bash 3.2 is still on macOS and how to get Bash 4 with Homebrew
+* https://www.shellcheck.net/ can offer suggestions to do shell scripting better
+* https://github.com/lovesegfault/beautysh installed via pip is helpful to format shell script files from the command line
--- esp_influx.sh
+++ esp_influx.sh
@@ -1,119 +1,110 @@
-#!/usr/bin/sh
+#!/usr/local/bin/bash
+# macOS `sh` is located at /bin/sh for 3.2 or /usr/local/bin/bash if installed with Homebrew or /usr/bin/bash for *nix
 
-#This script pulls data from Arduino temperature sensors that publish their data to self-hosted web pages
+# This script pulls data from Arduino ESP8266 sensors that publish their data to self-hosted web pages
 
-#Set NODE IP addresses
-NODEIP1="10.1.1.34" #34 is now the outside NodeMCU with PMS7003 and BME280 (nodemcu3 data feed)
-NODEIP2="10.1.1.35" #35 is now the living room NodeMCU with a BMP280 and DHT22 (nodemcu1 data feed)
-NODEIP3="10.1.1.36" #36 is now the inside bedroom NodeMCU with a BME280 (nodemcu2 data feed)
+# https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euo pipefail
 
+# https://linuxhint.com/bash_loop_list_strings/
+declare -a EspIpArray=(
+  "10.1.1.32" # living room NodeMCU with a BMP280, DHT22 (nodemcu1 data feed)
+"10.1.1.36" # inside bedroom GeekCreit with a BME280, DHT22 (nodemcu2 data feed)
+"10.1.1.34" # external-facing vent NodeMCU with PMS7003, BME280, DHT22 (nodemcu3 data feed)
+)
 
-#process first NodeMCU listed for data feed 3
-IN=`wget -qO- http://$NODEIP1` #get comma-delimited data from first NodeMCU
-set -- "$IN"
-IFS=","; declare -a PARSED=($*) #IFS separates a string into an array based on , delimiter
-echo "Array: ${PARSED[@]}" #print entire array result to output
-if [ ${#PARSED[@]} -eq 0 ] ; then #check to make sure the array has elements.
-    echo "wget parsing failed, is the URL correct?"
-else
-    #print values to console
-    echo "Humidity: ${PARSED[0]}"
-    echo "Temperature C: ${PARSED[1]}" 
-    echo "Temperature F: ${PARSED[2]}" 
-    echo "Pressure (inHg): ${PARSED[3]}" 
-    echo "PM1.0: ${PARSED[4]}" 
-    echo "PM2.5: ${PARSED[5]}"
-    echo "PM10: ${PARSED[6]}" 
-    echo ""
-    #Write the data to the database if it is not "nan" and >0. Spaces inside brackets are important. BC can do float comparison
-    if [ "${PARSED[0]}" != "nan" ] && \
-        [ "${PARSED[1]}"  != "nan" ] && \
-        [ "${PARSED[2]}" != "nan" ] && \
-        [ "${PARSED[3]}" != "nan" ] && \
-        [ "${PARSED[4]}" != "nan" ] && \
-        [ "${PARSED[5]}" != "nan" ] && \
-        [ "${PARSED[6]}" != "nan" ] && \
-        (( $(bc <<< "${PARSED[0]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[1]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[2]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[3]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[4]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[5]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[6]} > 0.0") )) ; then
-        # Submit all values as one row to InfluxDB
-        curl -i -XPOST 'http://10.1.1.7:8086/write?db=local_reporting' --data-binary \
-        "environment,host=nodemcu3 humidity=${PARSED[0]},temperaturec=${PARSED[1]},temperaturef=${PARSED[2]},pressurehg=${PARSED[3]},pm100=${PARSED[4]},pm250=${PARSED[5]},pm1000=${PARSED[6]}"
-    else
-        echo "One or more values nan or 0, skipping submission"
-    fi
+declare -a EspDestArray=(
+  "nodemcu1" # living room NodeMCU with a BMP280, DHT22 (nodemcu1 data feed)
+"nodemcu2" # inside bedroom GeekCreit with a BME280, DHT22 (nodemcu2 data feed)
+"nodemcu3" # external-facing vent NodeMCU with PMS7003, BME280, DHT22 (nodemcu3 data feed)
+)
+
+# Iterate the string array using for loop
+# https://stackoverflow.com/questions/8880603/loop-through-an-array-of-strings-in-bash
+# get length of an array
+arraylength=${#EspIpArray[@]}
+
+# use for loop to read all values and indexes
+for (( i=1; i<${arraylength}+1; i++ ));
+do
+  echo $i "/" ${arraylength} ":" ${EspIpArray[$i-1]} "to" ${EspDestArray[$i-1]}
+  # https://stackoverflow.com/questions/3742983/how-to-get-the-contents-of-a-webpage-in-a-shell-variable
+  # --silent to hide the download prgress from the output
+  webdata=$(curl --silent ${EspIpArray[$i-1]})
+  # echo $webdata
+  # Bash 4 support required for readarray -t y <<<"$webdata".
+  readarray -t linesplitwebdata <<<"$webdata"
+  # make sure the HTTP response is the proper length
+  # https://stackoverflow.com/questions/13101621/checking-if-length-of-array-is-equal-to-a-variable-in-bash
+  # https://stackoverflow.com/questions/9146136/check-if-file-exists-and-continue-else-exit-in-bash
+if [ ! "${#linesplitwebdata[@]}" -eq "2" ]; then
+    echo "Expected 2 lines of HTTP output, got:" ${#linesplitwebdata[@]}
+    exit 0
 fi
+  # https://helpmanual.io/builtin/readarray/ -d specifies comma delimiter, -t removes trailing delimiter
+  readarray -d , -t headercsvsplit <<<"${linesplitwebdata[0]}"
+  readarray -d , -t datacsvsplit <<<"${linesplitwebdata[1]}"
 
 
-#process second NodeMCU listed for data feed 1
-IN=`wget -qO- http://$NODEIP2` #get comma-delimited data from first NodeMCU
-set -- "$IN"
-IFS=","; declare -a PARSED=($*) #IFS separates a string into an array based on , delimiter
-echo "Array: ${PARSED[@]}" #print entire array result to output
-if [ ${#PARSED[@]} -eq 0 ] ; then #check to make sure the array has elements.
-    echo "wget parsing failed, is the URL correct?"
-else
-    #print values to console
-    echo "Humidity: ${PARSED[0]}"
-    echo "Temperature C: ${PARSED[1]}" 
-    echo "Temperature F: ${PARSED[2]}" 
-    echo "Pressure (inHg): ${PARSED[3]}" 
-    echo "PM1.0: ${PARSED[4]}" 
-    echo "PM2.5: ${PARSED[5]}"
-    echo "PM10: ${PARSED[6]}" 
-    echo ""
-    #Write the data to the database if it is not "nan" and >0. Spaces inside brackets are important. BC can do float comparison
-    if [ "${PARSED[0]}" != "nan" ] && \
-        [ "${PARSED[1]}"  != "nan" ] && \
-        [ "${PARSED[2]}" != "nan" ] && \
-        [ "${PARSED[3]}" != "nan" ] && \
-        (( $(bc <<< "${PARSED[0]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[1]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[2]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[3]} > 0.0") )) ; then
-        # Submit all values as one row to InfluxDB
-        curl -i -XPOST 'http://10.1.1.7:8086/write?db=local_reporting' --data-binary \
-        "environment,host=nodemcu1 humidity=${PARSED[0]},temperaturec=${PARSED[1]},temperaturef=${PARSED[2]},pressurehg=${PARSED[3]}"
-    else
-        echo "One or more values nan or 0, skipping submission"
+  innerArrayLength=${#headercsvsplit[@]}
+
+      areanynan=false
+    areanyinf=false
+    areanynegative=false
+
+    # https://stackoverflow.com/questions/15691942/print-array-elements-on-separate-lines-in-bash
+    #printf '%s\n' "${datacsvsplit[@]}"
+
+  # use for loop to read all names and values
+  for (( j=1; j<${innerArrayLength}+1; j++ ));
+  do
+    # trim whitespace https://stackoverflow.com/questions/369758/how-to-trim-whitespace-from-a-bash-variable
+    headercsvsplit[$j-1]="$( echo ${headercsvsplit[$j-1]} | xargs echo -n)"
+    datacsvsplit[$j-1]="$( echo ${datacsvsplit[$j-1]} | xargs echo -n)"
+
+    currentHeaderValue=${headercsvsplit[$j-1]}
+    currentDataValue=${datacsvsplit[$j-1]}
+
+    echo $j "/" ${innerArrayLength} ": header" $currentHeaderValue "value" $currentDataValue
+
+    # check that contents is numeric and above zero
+    if [ "$currentDataValue" = "nan" ]; then
+      echo "NAN value found for header:" $currentHeaderValue
+      areanynan=true
     fi
-fi
 
-
-#process third NodeMCU listed for data feed 2
-IN=`wget -qO- http://$NODEIP3` #get comma-delimited data from first NodeMCU
-set -- "$IN"
-IFS=","; declare -a PARSED=($*) #IFS separates a string into an array based on , delimiter
-echo "Array: ${PARSED[@]}" #print entire array result to output
-if [ ${#PARSED[@]} -eq 0 ] ; then #check to make sure the array has elements.
-    echo "wget parsing failed, is the URL correct?"
-else
-    #print values to console
-    echo "Humidity: ${PARSED[0]}"
-    echo "Temperature C: ${PARSED[1]}" 
-    echo "Temperature F: ${PARSED[2]}" 
-    echo "Pressure (inHg): ${PARSED[3]}" 
-    echo "PM1.0: ${PARSED[4]}" 
-    echo "PM2.5: ${PARSED[5]}"
-    echo "PM10: ${PARSED[6]}" 
-    echo ""
-    #Write the data to the database if it is not "nan" and >0. Spaces inside brackets are important. BC can do float comparison
-    if [ "${PARSED[0]}" != "nan" ] && \
-        [ "${PARSED[1]}"  != "nan" ] && \
-        [ "${PARSED[2]}" != "nan" ] && \
-        [ "${PARSED[3]}" != "nan" ] && \
-        (( $(bc <<< "${PARSED[0]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[1]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[2]} > 0.0") )) && \
-        (( $(bc <<< "${PARSED[3]} > 0.0") )) ; then
-        # Submit all values as one row to InfluxDB
-        curl -i -XPOST 'http://10.1.1.7:8086/write?db=local_reporting' --data-binary \
-        "environment,host=nodemcu2 humidity=${PARSED[0]},temperaturec=${PARSED[1]},temperaturef=${PARSED[2]},pressurehg=${PARSED[3]}"
-    else
-        echo "One or more values nan or 0, skipping submission"
+    if [ "$currentDataValue" = "inf" ]; then
+      echo "INF value found for header:" $currentHeaderValue
+      areanyinf=true
     fi
-fi
+
+        if (( $(bc <<< "$currentDataValue < 0.0") )) ; then
+      echo "Subzero value found for header:" $currentHeaderValue "value" $currentDataValue
+      areanynegative=true
+    fi
+  done
+
+  if ( $areanynan ) || ( $areanyinf ) || ( $areanynegative ) ; then
+    echo "Problematic values found, skipping submission"
+  else
+
+    # Map the input values to output values
+    humidity=${datacsvsplit[3]}
+    if [ "$humidity" = "inf" ]; then
+    humidity=${datacsvsplit[0]}
+    fi
+    temperaturec=${datacsvsplit[4]}
+        temperaturef=${datacsvsplit[5]}
+            pressurehg=${datacsvsplit[7]}
+                pm100=${datacsvsplit[8]}
+                pm250=${datacsvsplit[9]}
+                pm1000=${datacsvsplit[10]}
+
+    # Submit all values as one record to InfluxDB
+        curl -i -XPOST 'http://10.1.1.7:8086/write?db=local_reporting' --data-binary \
+        "environment,host=${EspDestArray[$i-1]} humidity=$humidity,temperaturec=$temperaturec,temperaturef=$temperaturef,pressurehg=$pressurehg,pm100=$pm100,pm250=$pm250,pm1000=$pm1000"
+
+  fi
+
+done
+
