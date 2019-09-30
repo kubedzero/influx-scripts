8a860bd934e9f6f60f43d9448ef2def9b7735f34
KZ <kubedzero@gmail.com>
2019-09-29 19:36:17 -0700

    Add CLI printouts and update shebang and path for CentOS

--- speedtest_influx.sh
+++ speedtest_influx.sh
@@ -1,4 +1,4 @@
-#!/usr/local/bin/bash
+#!/usr/local/sbin/bash
 # macOS `sh` is located at /bin/sh for 3.2 or /usr/local/bin/bash if installed with Homebrew
 # *nix is /usr/bin/bash but I installed bash 5.0 manually to /usr/local/sbin/bash on CentOS
 
@@ -11,12 +11,16 @@ set -euo pipefail
 # Run the speedtest using the `speedtest-cli` installed by pip at /opt/rh/rh-python36/root/usr/bin/speedtest-cli
 # https://github.com/sivel/speedtest-cli/releases
 # Determine which servers by using `speedtest-cli --list`
-speedtestresult=$(speedtest-cli --simple --server 603 --server 5754 --server 17587 --server 18531)
+speedtestversion=$(/opt/rh/rh-python36/root/usr/bin/speedtest-cli --version | awk '/speedtest-cli/{print $2}')
+echo "Running speedtest-cli version $speedtestversion which can take ~10s"
+speedtestresult=$(/opt/rh/rh-python36/root/usr/bin/speedtest-cli --simple --server 603 --server 5754 --server 17587 --server 18531)
 
 # Use `awk` to get the numerical values from the lines
 ping=$(echo "$speedtestresult" | awk '/Ping/{print $2}')
 download=$(echo "$speedtestresult" | awk '/Download/{print $2}')
 upload=$(echo "$speedtestresult" | awk '/Upload/{print $2}')
+
+echo "Ping was $ping ms and download was $download Mbit/s and upload was $upload Mbit/s. Sending to InfluxDB..."
 
 #Write to the database. Separate calls since each has its own metric value as well as value.
 /usr/bin/curl -i -XPOST 'http://10.1.1.7:8086/write?db=local_reporting' --data-binary "speedtest,metric=ping value=$ping"
